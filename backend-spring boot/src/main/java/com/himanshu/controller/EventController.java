78aaf80f6d32f40fe9504fc17d13c492d7200dce
himanshu <hg1480144@gmail.com>
2026-02-24 01:21:21 +0530

    Add pending project files

--- backend-spring boot/src/main/java/com/himanshu/controller/EventController.java
+++ backend-spring boot/src/main/java/com/himanshu/controller/EventController.java
@@ -1,25 +1,18 @@
 package com.himanshu.controller;
 
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.himanshu.dto.ApiResponse;
 import com.himanshu.dto.request.CreateEventRequest;
 import com.himanshu.dto.response.EventDto;
-import com.himanshu.mapper.EventMapper;
 import com.himanshu.exception.RestaurantException;
+import com.himanshu.mapper.EventMapper;
 import com.himanshu.model.entity.Events;
 import com.himanshu.service.EventsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,6 +21,7 @@ public class EventController {
 
 	private final EventsService eventService;
 	private final EventMapper eventMapper;
+
 
 
 
